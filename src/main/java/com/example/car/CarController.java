c9bcd1b892438296bf298f93c1e14082e81b028e
paritalasai <paritalasai9@gmail.com>
2017-03-07 16:34:04 +0530

    #API#

--- src/main/java/com/example/car/CarController.java
+++ src/main/java/com/example/car/CarController.java
@@ -3,7 +3,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
