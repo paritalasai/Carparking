d41b3546d9f89446f90a9f63c741729adb219f5d
paritalasai <paritalasai9@gmail.com>
2017-03-07 17:50:45 +0530

    #API#

--- src/main/java/com/example/car/CarRepository.java
+++ src/main/java/com/example/car/CarRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.example.model.Car;
-
 public interface CarRepository extends JpaRepository<Car, Integer> { 
 
 	Car findBycarnumber(long carnumber);
