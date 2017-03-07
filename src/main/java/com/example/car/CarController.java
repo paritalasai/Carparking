39a22268c781c73cfd6f1243ecbca755f5eaca20
paritalasai <paritalasai9@gmail.com>
2017-03-07 16:39:12 +0530

    #API#

--- src/main/java/com/example/car/CarController.java
+++ src/main/java/com/example/car/CarController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import com.example.model.Car;
 import com.example.model.ParkingDetails;
 
