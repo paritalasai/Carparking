d408e1b4417638dd140762c905c7e185387719bb
paritalasai <paritalasai9@gmail.com>
2017-03-08 12:45:23 +0530

    #UNIT#API#UI#

--- src/main/java/com/example/car/Car.java
+++ src/main/java/com/example/car/Car.java
@@ -8,6 +8,7 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
+
 @Entity
 @Table(name="car")
 public class Car {
