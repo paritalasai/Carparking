105376b1a6a9e3bf44dd0bc6283344ba2aff877f
paritalasai <paritalasai9@gmail.com>
2017-03-08 15:24:16 +0530

    #API#

--- src/main/java/com/example/car/Car.java
+++ src/main/java/com/example/car/Car.java
@@ -7,7 +7,6 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-
 import javax.persistence.Transient;
 @Entity
 @Table(name="car")
